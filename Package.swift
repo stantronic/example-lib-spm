6cbd8bd411be0e150605230f9e701643aca0feed
git@github.com <steven.stanton@autocab.com>
2026-05-07 16:02:35 +0100

    update url

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ExampleLib",
-            url: "https://trialdedos0.jfrog.io/artifactory/autocabtrial-libs-snapshot/com/autocab/spikes/examplelib-spm/1.0.0-SNAPSHOT/examplelib-spm-1.0.0-20260506.141851-1.zip",
+            url: "https://trialdedos0.jfrog.io/artifactory/autocabtrial-libs-snapshot-local/com/autocab/spikes/examplelib-spm/1.0.0-SNAPSHOT/examplelib-spm-1.0.0-20260507.145217-2.zip",
             checksum: "77802113c294267c964e1a8d6e3ffd75b5382686b98e545936ae41266e6a5d86"
         ),
         .target(name: "ExampleLibTarget", dependencies: [.target(name: "ExampleLib")]),
