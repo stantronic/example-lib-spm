ebc2918c8bf839ff061838b8c00a8080d449d91c
git@github.com <steven.stanton@autocab.com>
2026-05-07 13:59:57 +0100

    clean commit

--- Package.swift
+++ Package.swift
@@ -16,5 +16,5 @@ let package = Package(
             url: "https://trialdedos0.jfrog.io/artifactory/autocabtrial-libs-snapshot/com/autocab/spikes/examplelib-spm/1.0.0-SNAPSHOT/examplelib-spm-1.0.0-20260506.141851-1.zip",
             checksum: "c548b7ed168bff9b5992af6a734b1f24da98c79e059079a222dbae6ed2df2ffc"
         ),
-        .target(name: "ExampleLibTarget", dependencies: ["ExampleLib"]),
+        .target(name: "ExampleLibTarget", dependencies: [.target(name: "ExampleLib")]),
     ])
