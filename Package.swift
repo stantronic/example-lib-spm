482cd1f5ac75e8e212c5c0544b4a2375dbe36bf8
git@github.com <steven.stanton@autocab.com>
2026-05-14 14:11:45 +0100

    added more text

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ExampleLib",
-            url: "https://trialdedos0.jfrog.io/artifactory/autocabtrial-libs-snapshot-local/com/autocab/spikes/examplelib-spm/1.0.0-SNAPSHOT/examplelib-spm-1.0.0-20260514.102811-5.zip",
+            url:"https://trialdedos0.jfrog.io/artifactory/autocabtrial-libs-snapshot-local/com/autocab/spikes/examplelib-spm/1.0.0-SNAPSHOT/examplelib-spm-1.0.0-20260514.130826-6.zip",
             checksum: "4de90791084b5106ba6a3959197237016db90364a1bf6b8eb71bd1fe5be6aab8"
         ),
         .target(name: "ExampleLibTarget", dependencies: [.target(name: "ExampleLib")]),
